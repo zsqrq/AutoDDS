f0a09a06c82b2d12835600c02bf5c900e883568b
zsqrq <wz527193449@163.com>
2024-01-29 19:39:42 +0800

    add some test codes

--- test/testconpile.cpp
+++ test/testconpile.cpp
@@ -3,6 +3,32 @@
 //
 #include <iostream>
 #include "boost/config/config.hpp"
+#include "boost/config/config/compiler/gcc.hpp"
+union Data {
+  int i;
+  float f;
+  char c;
+};
 int main() {
+
+  Data data;
+  data.i = 10;
+  std::cout << data.i << std::endl;
+  std::cout << data.f << std::endl;
+  std::cout << sizeof(data) << std::endl;
+  std::cout << sizeof(int) << std::endl;
+  std::cout << sizeof(float) << std::endl;
+  data.f = 3.14159;
+  std::cout << data.i << std::endl;
+  std::cout << data.f << std::endl;
+  std::cout << sizeof(data) << std::endl;
+  data.c = 'a';
+  std::cout << data.i << std::endl;
+  std::cout << data.f << std::endl;
+  std::cout << data.c << std::endl;
+  std::cout << sizeof(data) << std::endl;
+  std::cout << sizeof(int) << std::endl;
+  std::cout << sizeof(float) << std::endl;
+
   return 0;
 }
