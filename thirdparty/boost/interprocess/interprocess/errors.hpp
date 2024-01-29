050ba1ef970531a86ce3c5af4ecb3e6bb3210d3c
wz527193449@163.com <wz527193449@163.com>
2024-01-29 23:46:00 +0800

    add interprocess codes

---
+++ thirdparty/boost/interprocess/interprocess/errors.hpp
@@ -0,0 +1,40 @@
+//
+// Created by wz on 24-1-29.
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Ion Gaztanaga 2005-2012. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/interprocess for documentation.
+//
+// Parts of this code are taken from boost::filesystem library
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (C) 2002 Beman Dawes
+//  Copyright (C) 2001 Dietmar Kuehl
+//  Use, modification, and distribution is subject to the Boost Software
+//  License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy
+//  at http://www.boost.org/LICENSE_1_0.txt)
+//
+//  See library home page at http://www.boost.org/libs/filesystem
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef AUTODDS_THIRDPARTY_BOOST_INTERPROCESS_INTERPROCESS_ERRORS_HPP_
+#define AUTODDS_THIRDPARTY_BOOST_INTERPROCESS_INTERPROCESS_ERRORS_HPP_
+
+#ifndef BOOST_CONFIG_HPP
+#  include "boost/config/config.hpp"
+#endif
+#if defined(BOOST_HAS_PRAGMA_ONCE)
+#  pragma once
+#endif
+#include "boost/interprocess/interprocess/detail/config_begin.hpp"
+#include "boost/interprocess/interprocess/detail/workaround.hpp"
+#include <string>
+
+
+#endif //AUTODDS_THIRDPARTY_BOOST_INTERPROCESS_INTERPROCESS_ERRORS_HPP_
