fb45a315041ae1d261ac46678e20af3be49ff090
zsqrq <wz527193449@163.com>
2024-02-09 13:02:09 +0800

    add integral constant codes

--- libs/type_traits/integral_constant.hpp
+++ libs/type_traits/integral_constant.hpp
@@ -5,13 +5,75 @@
 #ifndef AUTODDS_LIBS_TYPE_TRAITS_INTEGRAL_CONSTANT_HPP_
 #define AUTODDS_LIBS_TYPE_TRAITS_INTEGRAL_CONSTANT_HPP_
 
-#ifndef  AUTODDS_CONFIG_HPP
-#  include "libs/config/config.hpp"
-#endif
-#
 
-#if defined (AUTODDS_HAS_PRAGMA_ONCE)
-#  pragma once
-#endif
+#include "libs/config/config.hpp"
+#include "libs/config/workaround.hpp"
+
+namespace mpl_ {
+
+template <bool B> struct bool_;
+template <class I, I val> struct integral_c;
+struct integral_c_tag;
+
+}
+
+namespace autodds {
+namespace mpl {
+
+using ::mpl_::bool_;
+using ::mpl_::integral_c;
+using ::mpl_::integral_c_tag;
+
+}
+}
+
+namespace autodds {
+namespace libs {
+
+template <typename T, T val>
+struct integral_constant
+{
+  typedef mpl::integral_c_tag          tag;
+  typedef T                            value_type;
+  typedef integral_constant<T, val>    type;
+  static const T value = val;
+
+  operator const mpl::integral_c<T,val>& () const
+  {
+    static const char data[sizeof(long )] = {0};
+    static const void* pdata = data;
+    return *(reinterpret_cast<const mpl::integral_c<T,val>*>(pdata));
+  }
+  AUTODDS_CONSTEXPR operator T() const { return val; }
+};
+
+template <typename T, T val>
+T const integral_constant<T, val>::value;
+
+template <bool val>
+struct integral_constant<bool, val>
+{
+  typedef mpl::integral_c_tag tag;
+  typedef bool value_type;
+  typedef integral_constant<bool, val> type;
+  static const bool value = val;
+
+  operator const mpl::bool_<val>& ()const
+  {
+    static const char data[sizeof(long)] = { 0 };
+    static const void* pdata = data;
+    return *(reinterpret_cast<const mpl::bool_<val>*>(pdata));
+  }
+  AUTODDS_CONSTEXPR operator bool()const { return val; }
+};
+
+template <bool val>
+bool const integral_constant<bool, val>::value;
+
+typedef integral_constant<bool, true> true_type;
+typedef integral_constant<bool, false> false_type;
+
+}
+}
 
 #endif //AUTODDS_LIBS_TYPE_TRAITS_INTEGRAL_CONSTANT_HPP_
