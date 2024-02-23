0fdcb7a848a2291504c94b4258313200cc0aa105
zsqrq <wz527193449@163.com>
2024-02-23 17:57:45 +0800

    add some codes to managed_open_or_create_impl

---
+++ libs/interprocess/detail/interprocess_tester.hpp
@@ -0,0 +1,17 @@
+//
+// Created by wz on 24-2-23.
+//
+
+#ifndef AUTODDS_LIBS_INTERPROCESS_DETAIL_INTERPROCESS_TESTER_HPP_
+#define AUTODDS_LIBS_INTERPROCESS_DETAIL_INTERPROCESS_TESTER_HPP_
+
+#ifndef  AUTODDS_CONFIG_HPP
+#  include "libs/config/config.hpp"
+#endif
+#
+
+#if defined (AUTODDS_HAS_PRAGMA_ONCE)
+#  pragma once
+#endif
+
+#endif //AUTODDS_LIBS_INTERPROCESS_DETAIL_INTERPROCESS_TESTER_HPP_
--- libs/interprocess/detail/managed_open_or_create_impl.hpp
+++ libs/interprocess/detail/managed_open_or_create_impl.hpp
@@ -16,5 +16,60 @@
 
 #include "libs/interprocess/detail/config_begin.hpp"
 #include "libs/interprocess/detail/os_thread_functions.hpp"
+#include "libs/interprocess/detail/os_file_functions.hpp"
+#include "libs/interprocess/creation_tags.hpp"
+#include "libs/interprocess/mapped_region.hpp"
+#include "libs/interprocess/detail/utilities.hpp"
+#include "libs/interprocess/detail/type_traits.hpp"
+#include "libs/interprocess/detail/atomic.hpp"
+#include "libs/interprocess/detail/interprocess_tester.hpp"
+#include "libs/interprocess/detail/mpl.hpp"
+#include "libs/interprocess/permissions.hpp"
+#include "libs/container/detail/type_traits.hpp"
+#include "libs/interprocess/sync/spin/wait.hpp"
+#include "libs/interprocess/detail/timed_utils.hpp"
+#include "libs/move/move.hpp"
+#include "libs/common/cstdint.hpp"
+
+namespace autodds {
+namespace libs {
+namespace interprocess {
+namespace ipcdetail {
+
+template <bool StoreDevice, typename DeviceAbstraction>
+class managed_open_or_create_impl_device_holder
+{
+ public:
+  DeviceAbstraction& get_device()
+  {
+    static DeviceAbstraction device;
+    return device;
+  }
+
+  const DeviceAbstraction& get_device() const
+  {
+    static DeviceAbstraction device;
+    return device;
+  }
+};
+
+template <typename DeviceAbstraction>
+class managed_open_or_create_impl_device_holder<true, DeviceAbstraction>
+{
+ public:
+  DeviceAbstraction &get_device()
+  {  return dev; }
+
+  const DeviceAbstraction &get_device() const
+  {  return dev; }
+
+ private:
+  DeviceAbstraction dev;
+};
+
+} // namespace ipcdetail
+} // namespace interprocess
+} // namespace libs
+} // namespace autodds
 
 #endif //AUTODDS_LIBS_INTERPROCESS_DETAIL_MANAGED_OPEN_OR_CREATE_IMPL_HPP_
