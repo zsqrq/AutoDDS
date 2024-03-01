a454d68854757f148289c8bed9ccced5c4d169a1
zsqrq <wz527193449@163.com>
2024-03-01 17:46:35 +0800

    add container codes

--- libs/container/detail/iterator.hpp
+++ libs/container/detail/iterator.hpp
@@ -18,4 +18,73 @@
 #include "libs/move/utility_core.hpp"
 #include "libs/container/detail/mpl.hpp"
 
+namespace autodds {
+namespace libs {
+namespace container {
+
+using ::autodds::libs::intrusive::iterator_traits;
+using ::autodds::libs::intrusive::iter_difference;
+using ::autodds::libs::intrusive::iter_category;
+using ::autodds::libs::intrusive::iter_value;
+using ::autodds::libs::intrusive::iter_size;
+using ::autodds::libs::intrusive::iterator_distance;
+using ::autodds::libs::intrusive::iterator_udistance;
+using ::autodds::libs::intrusive::iterator_advance;
+using ::autodds::libs::intrusive::iterator_uadvance;
+using ::autodds::libs::intrusive::make_iterator_advance;
+using ::autodds::libs::intrusive::make_iterator_uadvance;
+using ::autodds::libs::intrusive::iterator;
+using ::autodds::libs::intrusive::iterator_enable_if_tag;
+using ::autodds::libs::intrusive::iterator_disable_if_tag;
+using ::autodds::libs::intrusive::iterator_arrow_result;
+
+template <typename Container>
+class back_emplacer
+{
+ private:
+  Container& container;
+
+ public:
+  typedef std::output_iterator_tag    iterator_category;
+  typedef void                        value_type;
+  typedef void                        difference_type;
+  typedef void                        pointer;
+  typedef void                        reference;
+
+  back_emplacer(Container& other)
+      : container(other)
+  {}
+
+  template<class U>
+  back_emplacer& operator =( AUTODDS_FWD_REF(U) value)
+  {
+    container.emplace_back(autodds::libs::forward<U>(value));
+    return *this;
+  }
+  
+  back_emplacer& operator *() { return *this; }
+  back_emplacer& operator ++() { return *this; }
+  back_emplacer& operator ++(int) { return *this; }
+};
+
+#ifndef AUTODDS_CONTAINER_NO_CXX17_CTAD
+
+template<class InputIterator>
+using it_based_non_const_first_type_t = typename dtl::remove_const<typename iterator_traits<InputIterator>::value_type::first_type>::type;
+
+template<class InputIterator>
+using it_based_const_first_type_t = const typename dtl::remove_const<typename iterator_traits<InputIterator>::value_type::first_type>::type;
+
+template<class InputIterator>
+using it_based_second_type_t = typename iterator_traits<InputIterator>::value_type::second_type;
+
+template<class InputIterator>
+using it_based_value_type_t = typename iterator_traits<InputIterator>::value_type;
+
+#endif
+
+} // namespace container
+} // namespace libs
+} // namespace autodds
+
 #endif //AUTODDS_LIBS_CONTAINER_DETAIL_ITERATOR_HPP_
--- libs/container/detail/mpl.hpp
+++ libs/container/detail/mpl.hpp
@@ -14,4 +14,124 @@
 #  pragma once
 #endif
 
+#include "libs/container/detail/config_begin.hpp"
+#include "libs/container/detail/workaround.hpp"
+#include "libs/move/detail/type_traits.hpp"
+#include "libs/intrusive/detail/mpl.hpp"
+
+#include <cstddef>
+
+namespace autodds {
+namespace libs {
+namespace container {
+namespace dtl {
+
+using autodds::libs::move_detail::integral_constant;
+using autodds::libs::move_detail::true_type;
+using autodds::libs::move_detail::false_type;
+using autodds::libs::move_detail::enable_if_c;
+using autodds::libs::move_detail::enable_if;
+using autodds::libs::move_detail::enable_if_convertible;
+using autodds::libs::move_detail::disable_if_c;
+using autodds::libs::move_detail::disable_if;
+using autodds::libs::move_detail::disable_if_convertible;
+using autodds::libs::move_detail::is_convertible;
+using autodds::libs::move_detail::if_c;
+using autodds::libs::move_detail::if_;
+using autodds::libs::move_detail::identity;
+using autodds::libs::move_detail::bool_;
+using autodds::libs::move_detail::true_;
+using autodds::libs::move_detail::false_;
+using autodds::libs::move_detail::yes_type;
+using autodds::libs::move_detail::no_type;
+using autodds::libs::move_detail::bool_;
+using autodds::libs::move_detail::true_;
+using autodds::libs::move_detail::false_;
+using autodds::libs::move_detail::unvoid_ref;
+using autodds::libs::move_detail::and_;
+using autodds::libs::move_detail::or_;
+using autodds::libs::move_detail::not_;
+using autodds::libs::move_detail::enable_if_and;
+using autodds::libs::move_detail::disable_if_and;
+using autodds::libs::move_detail::enable_if_or;
+using autodds::libs::move_detail::disable_if_or;
+using autodds::libs::move_detail::remove_const;
+
+template <typename FirstType>
+struct select1st
+{
+  typedef FirstType type;
+
+  template <typename T>
+  AUTODDS_CONTAINER_FORCEINLINE const type& operator()(const T& x) const
+  {  return x.first;   }
+
+  template<class T>
+  AUTODDS_CONTAINER_FORCEINLINE type& operator()(T& x)
+  {  return const_cast<type&>(x.first);   }
+};
+
+template <typename T>
+struct void_t { typedef void type; };
+
+template <typename T, typename = void>
+struct is_transparent_base
+{
+  static const bool value = false;
+};
+
+template <typename T>
+struct is_transparent_base<T, typename void_t<typename T::is_transparent>::type>
+{
+  static const bool value = true;
+};
+
+template <typename T>
+struct is_transparent
+    : is_transparent_base<T>
+{};
+
+template <typename C, typename /*Dummy*/, typename R>
+struct enable_if_transparent
+    : autodds::libs::move_detail::enable_if_c<is_transparent<C>::value, R>
+{};
+
+#ifndef AUTODDS_CONTAINER_NO_CXX17_CTAD
+
+// void_t (void_t for C++11)
+template<typename...> using variadic_void_t = void;
+
+// Trait to detect Allocator-like types.
+template <typename Allocator, typename = void>
+struct is_allocator
+{
+  static const bool value = false;
+};
+
+template <typename T>
+T&& ctad_declval();
+
+template <typename Allocator>
+struct is_allocator < Allocator,
+                      variadic_void_t< typename Allocator::value_type
+                          , decltype(ctad_declval<Allocator&>().allocate(size_t{})) >>
+{
+  static const bool value = true;
+};
+
+template<class T>
+using require_allocator_t = typename enable_if_c<is_allocator<T>::value, T>::type;
+
+template<class T>
+using require_nonallocator_t = typename enable_if_c<!is_allocator<T>::value, T>::type;
+
+#endif
+
+} // namespace dtl
+} // namespace container
+} // namespace libs
+} // namespace autodds
+
+#include "libs/container/detail/config_end.hpp"
+
 #endif //AUTODDS_LIBS_CONTAINER_DETAIL_MPL_HPP_
